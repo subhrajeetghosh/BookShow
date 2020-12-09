6dbf6ad161286ec02d0398a68655fae839056d71
subhrajeet <subhrajeet.ankhi@gmail.com>
2020-12-09 12:36:50 +0530

    some

--- src/main/java/com/example/springbootapp/Entity/Book.java
+++ src/main/java/com/example/springbootapp/Entity/Book.java
@@ -4,18 +4,53 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "postgres")
 public class Book {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  int id;
+    @Column(name = "Name")
     private String name;
+    @Column(name = "Author")
     private String author;
+
+    public Book() {
+        super();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Book(int id, String name, String author) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+    }
 }
